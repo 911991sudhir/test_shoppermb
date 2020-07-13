fba6ff75692ef330c01a739f7f174fadbe58e7fa
911991sudhir <911991sudhir@gmail.com>
2020-07-13 19:00:55 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+
+ $(document).ready(function() {
+	 //alert('ready');
+	$('ul li a.navhm').smoothScroll();	
+	$('ul li a.navabt').smoothScroll();
+	$('ul li a.ctntus').smoothScroll();
+	
+		
+	
+	$('body').click(function() {
+		/* $('.navbar-nav').hide(); */
+	});
+	
+});
+
+
+// Can also be used with $(document).ready()
+$(window).load(function() {
+ 
+ });
